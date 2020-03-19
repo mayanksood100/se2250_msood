0f16b0bd4ad2df0fbfa021057772c77cb4967610
Mayank Sood <mayanksood100@gmail.com>
2020-03-19 01:50:48 -0400

    First Commit - Phase 2
    I added the dash combat move. I also added a game start screen and designed the tilemap layout for level2.

---
+++ SE2250 RPG Game/Project1P1[214]/Assets/__Scripts/Enemy_Static.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Static : MonoBehaviour
+{
+    public int HealthPoint = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HealthPoint <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        HealthPoint -= damage;
+    }
+}
---
+++ SE2250 RPG Game/Project1P1[214]/Assets/__Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+	public Image healthBar;
+	public float health;
+	public float startHealth;
+
+public void onTakeDamage(int damage)
+    {
+		health = health - damage;
+		healthBar.fillAmount = health / startHealth;
+    }
+
+}
